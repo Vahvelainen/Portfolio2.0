5bb8d59079826eea844df86ae2b3328cc7dfa1ff
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-01-08 16:16:46 +0200

    add GA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,17 @@
+<svelte:head>
+  {#if !import.meta.env.DEV}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-P8LTLNZ4MJ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-P8LTLNZ4MJ');
+    </script>
+  {/if}
+</svelte:head>
+
 <script>
   import '../app.css'
   import Nav from '@lib/navbar.svelte'
