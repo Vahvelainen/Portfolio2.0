da00718632d1d2b957579c33b594dcb784b4999f
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-01-08 14:25:47 +0200

    Set prerender and csr withouth ssr

---
+++ src/routes/work/+layout.js
@@ -0,0 +1,9 @@
+
+//All pages should be able to prerender
+export const prerender = true;
+
+//Ssr is not needed with prerendering everytihing
+export const ssr = false;
+
+//Client side renderign is acceptable
+export const csr = false;
